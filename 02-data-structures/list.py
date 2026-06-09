882f698b0a33a0afda147f9c917081957f857990
Ibra Keita <ibrahimkeitapro99@gmail.com>
2026-06-09 16:25:37 +0200

    exos 2.1 FAIT

---
+++ 02-data-structures/list.py
@@ -0,0 +1,24 @@
+notes = [13,20,200,35,90,17,14,53,60]
+
+#parcourir
+plusgrand= notes[0]
+
+for note in (notes): 
+    if note > plusgrand :
+        plusgrand = note
+    print(f"le plus grand nombre dans la liste {notes} est le nombre {plusgrand}")
+
+#ajouter à une liste = nom_list.append(19)
+#remplacer = nom_list[place dans la list] = nouveau chiffre
+
+#parcourir list =  for [nom de la  liste] in [nom de la  liste] : print [nom de la  liste]
+
+#for note in notes : 
+    #total = total + note
+#jai aditioner les valeurs de la liste
+
+#moyenne = int(total / len(notes))
+#print(moyenne)
+
+
+
